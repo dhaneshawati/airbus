e178b93ca810f5167735ed45dc5f486522f0d7c7
dhaneshawati <dawati66@gmail.com>
2023-02-26 21:23:04 +0530

    Pages created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { faPlaneDeparture } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { plane-departure } from '@fortawesome/free-solid-svg-icons'
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-8 App">
+      <FontAwesomeIcon icon={faPlaneDeparture} />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
