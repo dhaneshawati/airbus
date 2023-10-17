ebe171c5558b829a0cb1377f0add607126f95284
dhaneshawati <dawati66@gmail.com>
2023-10-17 18:53:47 +0530

    New Firebase Project added

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,23 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 // import { getFirestore } from "firebase/firestore";
 import { useState, useEffect } from "react";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAnqFv5DEB9MGnMgaI5R2FQbgTdRhwcSmA",
+//   authDomain: "airbus-1b698.firebaseapp.com",
+//   projectId: "airbus-1b698",
+//   storageBucket: "airbus-1b698.appspot.com",
+//   messagingSenderId: "50105786508",
+//   appId: "1:50105786508:web:1b9969b1f6576395b29ba9",
+//   measurementId: "G-20NDJX5MT7",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyAnqFv5DEB9MGnMgaI5R2FQbgTdRhwcSmA",
-  authDomain: "airbus-1b698.firebaseapp.com",
-  projectId: "airbus-1b698",
-  storageBucket: "airbus-1b698.appspot.com",
-  messagingSenderId: "50105786508",
-  appId: "1:50105786508:web:1b9969b1f6576395b29ba9",
-  measurementId: "G-20NDJX5MT7",
+  apiKey: "AIzaSyBtkSPUfyR_zBArSmRRi9x5rvNfx_TBNZA",
+  authDomain: "airbus-flightbooking.firebaseapp.com",
+  projectId: "airbus-flightbooking",
+  storageBucket: "airbus-flightbooking.appspot.com",
+  messagingSenderId: "349297786431",
+  appId: "1:349297786431:web:b9bf49e06050390530028a",
+  measurementId: "G-PR3RKPZEN9"
 };
 
 // Initialize Firebase
